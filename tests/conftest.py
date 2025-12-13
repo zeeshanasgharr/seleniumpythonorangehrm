38709a28c3f7f52c5d0b63de2177ace3c79b2371
Zeeshan Asghar <33282252+zeeshanasgharr@users.noreply.github.com>
2025-12-13 14:07:31 +1030

    Simplify headless Chrome options in tests
    Removed unnecessary Chrome options for headless mode.

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,11 +15,6 @@ def driver():
 
     # Headless Chrome for Jenkins
     options.add_argument("--headless=new")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-
     driver = webdriver.Chrome(options=options)
 
     driver.implicitly_wait(10)
